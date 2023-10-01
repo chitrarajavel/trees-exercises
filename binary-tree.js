b857437a8c2ca3db1ec06f46cad6f637bf0002c7
Chitra Rajavel <chitrarajavel@gmail.com>
2023-10-01 11:42:42 -0500

    solution

--- binary-tree.js
+++ binary-tree.js
@@ -1,75 +1,118 @@
 /** BinaryTreeNode: node for a general tree. */
 
 class BinaryTreeNode {
-  constructor(val, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
 
 class BinaryTree {
-  constructor(root = null) {
-    this.root = root;
-  }
+    constructor(root = null) {
+        this.root = root;
+    }
 
-  /** minDepth(): return the minimum depth of the tree -- that is,
-   * the length of the shortest path from the root to a leaf. */
+    /** minDepth(): return the minimum depth of the tree -- that is,
+     * the length of the shortest path from the root to a leaf. */
 
-  minDepth() {
+    minDepth() {
+        if (this.root === null) return 0;
 
-  }
+        function checkChildren(node) {
+            if (node.left === null && node.right === null) {
+                //No children
+                return 1;
+            } else if (node.left !== null && node.right !== null) {
+                //Two children - Get the lower of the two
+                return (
+                    Math.min(
+                        checkChildren(node.left),
+                        checkChildren(node.right)
+                    ) + 1
+                );
+            } else {
+                //One child
+                if (node.left === null) {
+                    return checkChildren(node.right) + 1;
+                } else if (node.right === null) {
+                    return checkChildren(node.left) + 1;
+                }
+            }
+        }
+        return checkChildren(this.root);
+    }
 
-  /** maxDepth(): return the maximum depth of the tree -- that is,
-   * the length of the longest path from the root to a leaf. */
+    /** maxDepth(): return the maximum depth of the tree -- that is,
+     * the length of the longest path from the root to a leaf. */
 
-  maxDepth() {
+    maxDepth() {
+        if (this.root === null) return 0;
 
-  }
+        function checkChildren(node) {
+            if (node.left === null && node.right === null) {
+                //No children
+                return 1;
+            } else if (node.left !== null && node.right !== null) {
+                //Two children - Get the higher of the two
+                return (
+                    Math.max(
+                        checkChildren(node.left),
+                        checkChildren(node.right)
+                    ) + 1
+                );
+            } else {
+                //One child
+                if (node.left === null) {
+                    return checkChildren(node.right) + 1;
+                } else if (node.right === null) {
+                    return checkChildren(node.left) + 1;
+                }
+            }
+        }
+        return checkChildren(this.root);
+    }
 
-  /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
-   * The path doesn't need to start at the root, but you can't visit a node more than once. */
+    /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
+     * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-  maxSum() {
+    maxSum() {
+        //Reference SB solution
+        if (this.root === null) return 0;
+        let maxSumVal = 0;
+        function checkChildren(node) {
+            if (node === null) return 0;
+            let leftSum = checkChildren(node.left);
+            let rightSum = checkChildren(node.right);
 
-  }
+            //current node + left node + right node
+            let branchSum = node.val + leftSum + rightSum;
+            maxSumVal = Math.max(maxSumVal, branchSum);
+            return Math.max(leftSum + node.val, rightSum + node.val);
+        }
+        checkChildren(this.root);
+        return maxSumVal;
+    }
 
-  /** nextLarger(lowerBound): return the smallest value in the tree
-   * which is larger than lowerBound. Return null if no such value exists. */
+    /** nextLarger(lowerBound): return the smallest value in the tree
+     * which is larger than lowerBound. Return null if no such value exists. */
 
-  nextLarger(lowerBound) {
+    nextLarger(lowerBound) {
+        let minVal = null;
 
-  }
+        function findMin(node) {
+            if (node === null) return;
+            if (minVal === null && node.val > lowerBound) minVal = node.val;
 
-  /** Further study!
-   * areCousins(node1, node2): determine whether two nodes are cousins
-   * (i.e. are at the same level but have different parents. ) */
-
-  areCousins(node1, node2) {
-
-  }
-
-  /** Further study!
-   * serialize(tree): serialize the BinaryTree object tree into a string. */
-
-  static serialize() {
-
-  }
-
-  /** Further study!
-   * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
-
-  static deserialize() {
-
-  }
-
-  /** Further study!
-   * lowestCommonAncestor(node1, node2): find the lowest common ancestor
-   * of two nodes in a binary tree. */
-
-  lowestCommonAncestor(node1, node2) {
-    
-  }
+            if (node.val < minVal && node.val > lowerBound) {
+                minVal = node.val;
+            }
+            if (node.left !== null) findMin(node.left);
+            if (node.right !== null) findMin(node.right);
+        }
+        findMin(this.root);
+        return minVal;
+    }
 }
 
-module.exports = { BinaryTree, BinaryTreeNode };
+module.exports = {BinaryTree, BinaryTreeNode};
